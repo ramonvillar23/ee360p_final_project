4d9aeff8626da5a3cc87d7e909b090963f4086f7
Zach Dewey <zdewey413@gmail.com>
2016-04-26 17:24:56 -0500

    Added VersionObject abstract class.

---
+++ src/lib/VersionObject.java
@@ -0,0 +1,28 @@
+public abstract class VersionObject
+{
+	private int version;
+	
+	public int getVersion()
+	{
+		return version;
+	}
+	
+	public void setVersion()
+	{
+		version++;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if(!(obj instanceof VersionObject))
+			return false;
+		else if(version == ((VersionObject)obj).getVersion())
+			return true;
+		return false;
+	}
+	
+	public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+}
