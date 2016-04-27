307b2d5ce666325ad6cb3a1ac41229c6f2bff54d
Jackson Haenchen <jacksonhaenchen@gmail.com>
2016-04-27 17:37:19 -0500

    Added timer class

--- src/runtime/Main.java
+++ src/runtime/Main.java
@@ -77,4 +77,26 @@ public class Main extends Monitor{
 		}
 		
 	}
+	
+	public class Timer {
+		private long startTime;
+		private long endTime;
+		
+		public Timer() {
+			startTime = 0;
+			endTime = 0;
+		}
+		
+		public void start() {
+			startTime = System.nanoTime();
+		}
+		
+		public void stop() { 
+			endTime = System.nanoTime();
+		}
+		
+		public long getTime() {
+			return endTime - startTime;
+		}
+	}
 }
