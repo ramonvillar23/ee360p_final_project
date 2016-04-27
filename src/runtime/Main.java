5a25b5a9712071eb1eee6c4955cab341a571e9f1
Jackson Haenchen <jacksonhaenchen@gmail.com>
2016-04-27 18:36:35 -0500

    Main uses a loop to find the average

--- src/runtime/Main.java
+++ src/runtime/Main.java
@@ -1,16 +1,11 @@
 package runtime;
 
 import java.util.ArrayList;
-import java.util.function.Predicate;
-
-import lib.IntThread;
-import lib.Monitor;
 import lib.Reader;
 import lib.Timer;
-import lib.VersionInteger;
 import lib.Writer;
 
-public class Main extends Monitor{
+public class Main{
 	public Main()
 	{
 		super();
@@ -19,28 +14,39 @@ public class Main extends Monitor{
 	public static void main(String[] args) {		
 		//IntThread thread1 = new IntThread(newInt, mon, 1);
 		//IntThread thread2 = new IntThread(newInt, mon, 2);
-		ReaderWriterImplicit rw = new ReaderWriterImplicit();
-		ArrayList<Thread> threads = new ArrayList<Thread>();
-		for(int i = 0; i < 1; i++){
-			threads.add(new Thread(new Reader(rw)));
-			threads.add(new Thread(new Writer(rw)));
-		}
-		
-		for(Thread t : threads){
-			t.start();
-		}
-		
-		Timer time = new Timer();
-		time.start();
-		
-		for(Thread t : threads){
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		ArrayList<Long> results = new ArrayList<Long>();
+		for(int j = 0; j < 10; j++){
+			ProducerConsumerImplicit pc = new ProducerConsumerImplicit();
+			ArrayList<Thread> threads = new ArrayList<Thread>();
+			for(int i = 0; i < 1; i++){
+				threads.add(new Thread(new Reader(rw)));
+				threads.add(new Thread(new Writer(rw)));
 			}
+			
+			for(Thread t : threads){
+				t.start();
+			}
+			
+			Timer time = new Timer();
+			time.start();
+			
+			for(Thread t : threads){
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			time.stop();
+			results.add(time.getTime());
+			//System.out.println("Completed in "+time.getTime()/1000000.0);
 		}
-		time.stop();
-		System.out.println("Completed in "+time.getTime()/1000000.0);
+		long sum = 0;
+		for(Long d : results){
+			sum += d;
+		}
+		System.out.println("Trial Average: "+(sum/results.size())/1000000.0);
+		
+		
 	}
 }
